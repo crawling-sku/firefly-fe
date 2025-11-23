e2c2e104f544bfc17e660cb613a7d3966be4355e
shinchaerin79 <shinchaerin79@gmail.com>
2025-11-23 17:57:07 +0900

    :heavy_plus_sign: Dependency: react-router-dom, axios, ESLint, Prettier 환경 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended", // Prettier와 충돌 방지
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  ignorePatterns: ["dist"],
+  rules: {
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "prettier/prettier": ["error"], // Prettier 규칙 위반 시 ESLint 에러
+  },
+};
